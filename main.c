3d567a9636558701b3ea8efc31da281c4945fc97
Adrien <a.bonnand@pacman.fr>
2023-11-21 15:42:49 +0100

    création de l'énumération choix joueur

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ struct Carte{
 };
 
 typedef struct listeCartes{
-    Carte carte[];
+    struct Carte carte[];
 };
 
 
+enum enum_color {CARTE=0,HIT=0,ARRETER=1,STAND=1,DOUBLE=2,ABANDONNER=3,SURREND=3};
