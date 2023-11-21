cfb8cef6e71663ee0a66963d955a682bf3ab316d
Thomas Grenet <77513514+bynexs@users.noreply.github.com>
2023-11-21 17:17:24 +0100

    2.1 Creation du deck

--- main.c
+++ main.c
@@ -2,14 +2,24 @@
 enum enum_color {CARREAU, PIQUE, COEUR, TREFLE};
 
 struct Carte{
-
-    int valeur; 
+    int valeur;
     enum enum_color type;
-
 };
 
 typedef struct listeCartes{
-    Carte carte[];
+    struct Carte carte[52];
 };
 
-
+int CreationDeck(){
+    struct listeCartes listecarte;
+    int compt  = 0;
+    for(int i = 0; i < 4; i++){
+        for(int j = 1; j <= 13; j++){
+            struct Carte carte;
+            carte.valeur = j;
+            carte.type = i;
+            listecarte.carte[compt] = carte;
+            compt ++;
+        }
+    }
+}
