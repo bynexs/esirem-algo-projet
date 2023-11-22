5588b65690be54b4ba704a5f6f7d7956dfe27651
Adrien <a.bonnand@pacman.fr>
2023-11-22 13:05:22 +0100

    création de l'affichage de la main

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 enum enum_color {CARREAU, PIQUE, COEUR, TREFLE};
 
-enum enum_color {CARTE=0,HIT=0,ARRETER=1,STAND=1,DOUBLE=2,ABANDONNER=3,SURREND=3};
+enum enum_joueur {CARTE=0,HIT=0,ARRETER=1,STAND=1,DOUBLE=2,ABANDONNER=3,SURREND=3};
 
 struct Carte{
 
@@ -13,13 +13,21 @@ struct Carte{
 };
 
 typedef struct listeCartes{
-    struct Carte carte[];
+    struct Carte carte[52];
 };
 
 
 typedef struct joueur{
-    struct listeCartes;
-    int = montant;
+    struct listeCartes listeCartes;
+    int montant;
     
 };
 
+void affichage_main(struct main main[]){
+    int i=0; 
+    
+    for(i=0;i<sizeof(main)+1;i++){
+        printf("Voici la carte %d :\n", i+1);
+        printf("Valeur : %d | Couleur : %d\n", main.valeur[i], main.type[i]);
+    }
+}
